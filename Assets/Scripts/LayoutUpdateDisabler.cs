dd87e3df4bf4e8e808e578dee42ac2f67944335a
samezi <d_viper@hotmail.co.jp>
2020-06-07 19:17:39 +0900

    1秒だけuDDのレイアウターを動かすように細工

---
+++ Assets/Scripts/LayoutUpdateDisabler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayoutUpdateDisabler : MonoBehaviour
+{
+    public MultipleMonitorLayouter multipleMonitorLayouter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisabledUpdateFrame());
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private IEnumerator DisabledUpdateFrame()
+    {
+        yield return new WaitForSeconds(1f);
+        multipleMonitorLayouter.updateEveryFrame = false;
+        yield return null;
+    }
+}
